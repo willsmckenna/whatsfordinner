4c236e257214d63d2a4d15632d748416e0ef60a4
willsmckenna <38638735+willsmckenna@users.noreply.github.com>
2021-04-28 18:11:59 -0500

    Update App.js

--- App.js
+++ App.js
@@ -16,9 +16,3 @@ const navigator = createStackNavigator(
 );
 
 export default createAppContainer(navigator);
-
-// Client ID
-//Stbump_8oTuq2efEDya1Fg
-
-//API Key
-//3Yco9sGiJ4ZEXsTUpkJ6YAauUWi81QIKmPPZn7VqVjzdUDhARW0P_gH0_QM4DzE1LRTwm_pHGAeFVMwy5oN45lQZ41ft92agK03W0q2kVawGRZIqW_3XqDfX0meHYHYx
