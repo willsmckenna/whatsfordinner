8b30c663aaefd419aa165802f428800189269b1d
Shatilla Prayer <cjsgamer@gmail.com>
2021-05-10 15:25:05 -0700

    removed api key

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,7 +4,6 @@ import { YELP_API_KEY } from '@env';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        //Authorization:`Bearer ${YELP_API_KEY}`
-        Authorization: 'Bearer Au-TEyW9ThTl0H0d23QWTFwART8jKhy0v1oA3XfGQaYMDuQuANT8gBNLMjRRB96K9xQM-Ux08C43gmIUoBgExtwiUjusyJq03GtD4X_yKFrph8jRr1CZ9kx_Lo2ZYHYx'
+        Authorization:`Bearer ${YELP_API_KEY}`
     }
 });
