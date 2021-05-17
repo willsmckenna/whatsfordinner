9ae23b666436845c896726f4a2836907b2b91efe
Wills <willsmckenna6@gmail.com>
2021-05-17 16:35:32 -0500

    restaurant sign up

--- src/screens/RestaurantProfile.js
+++ src/screens/RestaurantProfile.js
@@ -1,7 +1,18 @@
 import React, { useState } from 'react';
 import { Text, TextInput, Button, View, StyleSheet } from 'react-native';
+import { createRestaurant } from './graphql/mutations';
 
 const RestaurantProfile = ({ navigation }) => {
+    const [resDetail, setResDetail] = useState({});
+
+    const handleSubmit = () => {
+        try {
+            await API.graphql(graphqlOperation(createRestaurant, {input: resDetail}));
+            navigation.navigate('Search');
+        } catch (err) {
+            console.log('error creating restaurant:', err);
+        }
+    }
       return (
         <View style = {styles.container}>
             <Text style={styles.headingText}>Restaurant Information</Text>
@@ -10,33 +21,39 @@ const RestaurantProfile = ({ navigation }) => {
             <TextInput 
                 style={styles.input}
                 placeholder='e.g. St. Paris'
+                onChangeText={e => setResDetail({ ...resDetail, title: e.target.value })}
             />    
             <Text> Address </Text> 
             <TextInput 
                 style={styles.input}
                 placeholder='e.g. 401 N Central, Chicago, Il 60656'
+                onChangeText={e => setResDetail({ ...resDetail, address: e.target.value })}
             />     
             <Text> Telephone </Text> 
             <TextInput 
                 style={styles.input}
                 placeholder='e.g. 703-996-7185'
+                onChangeText={e => setResDetail({ ...resDetail, telephone: e.target.value })}
             />
             <Text> Hours </Text> 
             <TextInput 
                 style={styles.input}
                 placeholder='e.g. Everyday 10 a.m. to 9 p.m.'
+                onChangeText={e => setResDetail({ ...resDetail, hours: e.target.value })}
             />
             <Text> Reservations </Text> 
             <TextInput 
                 style={styles.input}
                 placeholder='e.g. opentable.com'
+                onChangeText={e => setResDetail({ ...resDetail, reservation: e.target.value })}
             />
             <Text> Order </Text> 
             <TextInput 
                 style={styles.input}
                 placeholder='e.g. ubereats.com, doordash.com, etc.'
+                onChangeText={e => setResDetail({ ...resDetail, order: e.target.value })}
             />
-            <Button title="Submit" onPress={() => navigation.navigate('Search')}/> 
+            <Button title="Submit" onPress={handleSubmit}/> 
         </View>   
       );
 }
