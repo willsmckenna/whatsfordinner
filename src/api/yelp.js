88bef720725a0c701bb68812973384124302a3d9
Cynthia Rangel <CRANGEL7@depaul.edu>
2021-06-01 15:12:08 -0500

    api key left in

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,6 @@ import { YELP_API_KEY } from '@env';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization:`9jj_4ARsBFCZyaP7gV5G30l3jYCziZ7Sc_opzuI0CBgdwXY4tCJUBsbdAt6r9geKskns7MKemOnW91oMtVYBg4rlYWHtlJHhaup47TtgNokhmpGHS0Uod0RVz_mZYHYx`
+        Authorization: `Bearer ${YELP_API_KEY}`
     }
 });
