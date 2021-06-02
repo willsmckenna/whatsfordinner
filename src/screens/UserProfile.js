a297bce5e5f6fd2e3898caa6f09809604cc1e7a7
Wills <willsmckenna6@gmail.com>
2021-06-02 18:27:47 -0500

    merging cindy's stuff to mine

--- src/screens/UserProfile.js
+++ src/screens/UserProfile.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button } from 'react-native';
-import {Authenticator} from 'aws-amplify-react-native';
+import { View, Text } from 'react-native';
+import { Authenticator } from 'aws-amplify-react-native';
 import Auth from '@aws-amplify/auth';
-import { getUserPreferences } from '../graphql/queries';
+import { listUsers } from '../graphql/queries';
 import Amplify, { API, graphqlOperation } from 'aws-amplify';
-import ListUserProfile from '../components/ListUserProfile';
+import ProfileDetails from '../components/ProfileDetails';
 
-const UserProfile = ( {navigation} ) => {
+const UserProfile = () => {
     const [userDetails, setUserDetails] = useState([]);
     const [errorMsg, setErrorMsg] = useState('');
 
@@ -14,32 +14,31 @@ const UserProfile = ( {navigation} ) => {
         async function fetchProfileData() {
             try {
                 const { id } = await Auth.currentUserInfo();
-                const userDetail = await API.graphql(graphqlOperation(getUserPreferences, { 
+                const userDetail = await API.graphql(graphqlOperation(listUsers, {
                     filter: {
-                        userId: { eq: id } 
+                        userId: { eq: id }
                     }
                 }));
-                let userList = [];
+
+                let useList = [];
                 const { data } = userDetail;
-                console.log(data);
-                const userProfile = data.listRestaurants.items[0];
-                const { username, foodPreferences } = userProfile;
-                userList.push(username, foodPreferences);
-                setUserDetails(userList);
-                console.log(userDetails);
-                
-            } catch (err){
-                console.log("error fetching data:" +  JSON.stringify(err));
-                setErrorMsg('No user food preferences found for your account. Click the add food preferences button to add to your account');
+                const userProfile = data.listUsers.items[0];
+                if (!userProfile) {
+                    setErrorMsg('You have not created a profile yet. Please create one.');
+                }
+                const { name, email,telephone,preferences,orders, reservations} = userProfile;
+                useList.push(name, email, telephone, preferences, orders, reservations );
+                setUserDetails(useList);
+            } catch (err) {
+                setErrorMsg('You have not created a profile yet. Please create one.');
+                console.log("error fetching data:" + err);
             }
         }
-      fetchProfileData();
+        fetchProfileData();
     }, []);
-
     return (
         <View>
-            {errorMsg ? <Text>{errorMsg}</Text> : <ListUserProfile userProfile={userDetails} />}
-            <Button title="Add Food Preferences" onPress={() => navigation.navigate('FoodSelectScreen')}/>
+            {errorMsg ? <Text> {errorMsg} </Text> : <ProfileDetails useProfile={userDetails} />}
         </View>
     );
 };
